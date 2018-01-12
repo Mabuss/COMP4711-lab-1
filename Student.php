99a231ec0f5fcfc13f385a54a01c20b64ea049a2
Jack Yang <jackguangyang@gmail.com>
2018-01-11 16:49:38 -0800

    Added comments to the Student class.

--- Student.php
+++ Student.php
@@ -6,8 +6,14 @@
  * Time: 4:19 PM
  */
 
+/**
+ * Class Student
+ */
 class Student
 {
+    /**
+     * Student constructor.
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -15,14 +21,27 @@ class Student
         $this->grades = array();
     }
 
+    /**
+     * Adds an email address.
+     * @param $which The email type.
+     * @param $address The address.
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Adds an grade.
+     * @param $grade the grade.
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /**
+     * Calculated the average grade for the student.
+     * @return float|int the average.
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -30,6 +49,10 @@ class Student
         return $total / count($this->grades);
     }
 
+    /**
+     * Converts the student to a string for printing.
+     * @return string the string conversion.
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
